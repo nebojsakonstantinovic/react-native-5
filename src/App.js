1d75d8c9c8348bc94df92ed1e0c1d09159b7b4f3
Nebojsa Konstantinovic <nkonstantinovic@spartagency.com>
2019-02-05 17:37:31 +0100

    init commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>Helllo!</Text>
+        </View>
+      </Provider>
+    );
+  }
+}
+
+export default App;
