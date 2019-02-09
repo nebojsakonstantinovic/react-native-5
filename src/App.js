312c3d26c35db1494d8e728d852f2da0c45bfd2d
Nebojsa Konstantinovic <nebojsa.konstantin@gmail.com>
2019-02-09 17:00:25 +0100

    firebase added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,21 @@ import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import firebase from 'firebase';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: "AIzaSyAFNJgcQSpul9-L4oFfEjmPsgnfh0MmghY",
+      authDomain: "manager-react-native-d7da1.firebaseapp.com",
+      databaseURL: "https://manager-react-native-d7da1.firebaseio.com",
+      projectId: "manager-react-native-d7da1",
+      storageBucket: "manager-react-native-d7da1.appspot.com",
+      messagingSenderId: "199260096210"
+    };
+    firebase.initializeApp(config);
+  }
+  
   render() {
     return (
       <Provider store={createStore(reducers)}>
